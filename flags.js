e73824d6d83d6289bcf70234f081240ddaa1a582
Lekkaiyaraja <lekkaiyarajaraj2020@gmail.com>
2024-02-28 21:37:45 +0530

    Rest Countries Name

---
+++ flags.js
@@ -0,0 +1,18 @@
+
+const req = new XMLHttpRequest();
+
+
+req.open("GET","https://restcountries.com/v3.1/all")
+req.send();
+req.onload=function(){
+    if(req.status>=200 && req.status<300){
+      const obj = JSON.parse(req.responseText);
+      for (let ind = 0; ind < obj.length; ind++) {
+        console.log(obj[ind].flags.png);
+      }
+         
+			console.log(data)
+ 
+     }
+     
+}
--- script.js
+++ script.js
@@ -11,15 +11,17 @@ const req = new XMLHttpRequest();
 
 // }
 req.open("GET","https://restcountries.com/v3.1/all")
-req.send()
-
-
+req.send();
 req.onload=function(){
     if(req.status>=200 && req.status<300){
       // console.log(req.responseText)
-      var data=JSON.parse(req.responseText);
+      const obj = JSON.parse(req.responseText);
+      for (let ind = 0; ind < obj.length; ind++) {
+        console.log(obj[ind].name.common);
+      }
          
 			console.log(data)
  
      }
+     
 }
